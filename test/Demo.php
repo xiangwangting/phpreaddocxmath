e51e2a20501bef3ab24557971d081d4133e74d84
“xiangwang” <344496982@qq.com>
2021-02-01 17:12:11 +0800

    tijia

--- test/Demo.php
+++ test/Demo.php
@@ -8,9 +8,8 @@ include_once('ImageDiyHandelDemo.php');
 
 $file_url = 'https://zjx-worker-files-develop.oss-cn-shanghai.aliyuncs.com/10071/uploads/OnlineOfficeFiles/1475a9f9d6eda0678ca6fa29030d69cb';
 //$file_url = 'https://zjx-worker-files-develop.oss-cn-shanghai.aliyuncs.com/10071/uploads/OnlineOfficeFiles/f97afb06d1864582433c5f7618cd3886';
-var_dump('--开始读取文件---');
+echo "--开始读取文件---\n";
 $docx = (new ReadDocxService())->setImgHandelClass(ImageDiyHandelDemo::class)->setFileUrl($file_url)->extractToHtml(
     'read_docx.html'
 );
-var_dump($docx);
-var_dump('--完成--');
+echo "--完成--\n";
