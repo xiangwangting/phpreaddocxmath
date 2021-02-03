4aaf7725fae3a5f7dc299dd7ae65ea1ba9d17c79
“xiangwang” <344496982@qq.com>
2021-02-03 18:03:34 +0800

    修改处理执行顺序

--- src/ExtractConfig.php
+++ src/ExtractConfig.php
@@ -20,11 +20,11 @@ class ExtractConfig
      * 转换配置
      */
     const CONFIG = [
-        SupExtract::class,
-        SubExtract::class,
-        FontExtract::class,
-        MathExtract::class,
-        ImgExtract::class,
+        ImgExtract::class,//图片必须最先处理
+        SupExtract::class,//上标必须在字体处理前
+        SubExtract::class,//下标必须在字体处理前
+        FontExtract::class,//字体处理
+        MathExtract::class,//公式必须在字体处理后
         TableExtract::class,
     ];
 }
